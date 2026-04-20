967620e95f21f8f7c800b23fac0fe8c9df8575c2
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2026-04-20 15:08:08 +0900

    groupListScreen ui title, subtitle 간격조정 및 스크린 색상 설정

--- lib/Screen/group_list_screen.dart
+++ lib/Screen/group_list_screen.dart
@@ -5,9 +5,15 @@ class GroupListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final rooms = [
-      {'name': 'group1', 'members': 2},
-      {'name': 'group2', 'members': 4},
+    final List<Map<String, dynamic>> rooms = [
+      {
+        'name': 'group1',
+        'members': ['유저1', '유저2'],
+      },
+      {
+        'name': 'group2',
+        'members': ['유저2', '유저3', '유저5'],
+      },
     ];
 
     return Scaffold(
@@ -15,16 +21,56 @@ class GroupListScreen extends StatelessWidget {
         title: const Text('Bababam'),
         actions: [IconButton(icon: const Icon(Icons.add), onPressed: () {})],
       ),
-      body: ListView.builder(
-        itemCount: rooms.length,
-        itemBuilder: (context, index) {
-          final room = rooms[index];
+      body: Container(
+        color: Color(0xFF0A0A0A),
+        child: ListView.builder(
+          itemCount: rooms.length,
+          padding: const EdgeInsets.all(12),
+          itemBuilder: (context, index) {
+            final room = rooms[index];
 
-          return ListTile(
-            title: Text(room['name'].toString()),
-            subtitle: Text('멤버 ${room['members']}명'),
-          );
-        },
+            return Container(
+              margin: const EdgeInsets.only(bottom: 12),
+              decoration: BoxDecoration(
+                color: Color(0xFF121212),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    blurRadius: 5,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      room['name'].toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      '${(room['members'] as List).length}명',
+                      style: const TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                subtitle: Text(
+                  (room['members'] as List).join(', '),
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white.withValues(alpha: 0.4),
+                    letterSpacing: -0.2,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
