79dc26e438f3571f30611559c8dd8691e460d4cc
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2026-04-22 21:56:39 +0900

    Dialog insetPadding 수치 조정

--- lib/Widget/confirm_dialog.dart
+++ lib/Widget/confirm_dialog.dart
@@ -16,20 +16,24 @@ class ConfirmDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: const Color(0xFF1A1A1A),
+      insetPadding: const EdgeInsets.symmetric(horizontal: 70),
       title: Text(
         title,
         style: const TextStyle(color: Colors.white, fontSize: 18),
       ),
       content: Text(message, style: const TextStyle(color: Colors.white70)),
+      actionsAlignment: MainAxisAlignment.spaceEvenly,
       actions: [
+        // cancel
         TextButton(
           onPressed: () => Navigator.pop(context),
           child: const Text('취소', style: TextStyle(color: Colors.grey)),
         ),
+        // ok
         TextButton(
           onPressed: () {
-            Navigator.pop(context); // 다이얼로그 닫기
-            onConfirm(); // 전달받은 생성 로직 실행
+            Navigator.pop(context);
+            onConfirm();
           },
           child: const Text('확인', style: TextStyle(color: Color(0xFF7C3AED))),
         ),
