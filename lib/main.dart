9627f1e2d50b1a1223512cc521e5c927b3f96525
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2026-04-27 16:37:13 +0900

    firebase DB 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:bababam_app/Screen/group_list_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const BababamApp());
 }
 
