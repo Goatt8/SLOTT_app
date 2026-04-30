69e8e1dd29e11ab8c7ce8b7c2956a66758427e31
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2026-04-30 13:49:48 +0900

    Remove unused hour slot feed model

--- lib/Model/hour_slot_feed.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bababam_app/Model/post.dart';
-
-class HourSlotFeed {
-  final int hourSlot;
-  final List<Post> posts;
-
-  const HourSlotFeed({required this.hour, required this.posts});
-}
