a5f022a509a780a0c59a9e55b7186fcc102eebbd
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2026-05-15 17:16:34 +0900

    CameraScreen - 카메라 촬영 로직 수정, 테스트 위해 실 기기 시뮬레이팅 연결

--- lib/Screen/camera_screen.dart
+++ lib/Screen/camera_screen.dart
@@ -16,11 +16,47 @@ class _CameraScreenState extends State<CameraScreen> {
   CameraController? _controller;
   List<CameraDescription>? _cameras;
   bool _isInitialized = false;
+  bool _isRecording = false;
+
+  String testVideoPath = 'assets/video/test_video.mp4';
 
   @override
   void initState() {
     super.initState();
     _initializeCamera();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _recordVideo() async {
+    if (_controller == null || !_controller!.value.isInitialized) {
+      print("컨트롤러가 없으므로 테스트 영상으로 즉시 이동합니다.");
+      _moveToPreviewScreen('assets/video/test_video.mp4');
+      return;
+    }
+
+    try {
+      await _controller!.startVideoRecording();
+      setState(() => _isRecording = true);
+
+      await Future.delayed(const Duration(seconds: 3));
+
+      if (_isRecording) {
+        final file = await _controller!.stopVideoRecording();
+        setState(() => _isRecording = false);
+
+        //MARK: test
+        _moveToPreviewScreen(testVideoPath);
+        //  _moveToPreviewScreen(file.path);
+      }
+    } catch (e) {
+      print("4초 자동 녹화 에러: $e");
+      setState(() => _isRecording = false);
+    }
   }
 
   //MARK: Initialize
@@ -29,7 +65,7 @@ class _CameraScreenState extends State<CameraScreen> {
       _cameras = await availableCameras();
 
       if (_cameras != null && _cameras!.isNotEmpty) {
-        _controller = CameraController(_cameras![0], ResolutionPreset.high);
+        _controller = CameraController(_cameras![0], ResolutionPreset.medium);
         await _controller!.initialize();
 
         if (!mounted) return;
@@ -38,86 +74,106 @@ class _CameraScreenState extends State<CameraScreen> {
         });
       } else {
         print("카메라 리스트가 비어있음");
-        _moveToPreviewScreen();
+        //MARK: test
+        // _moveToPreviewScreen(testVideoPath);
+
+        print("카메라 리스트가 비어있음 -> UI 점검을 위해 화면 유지");
+        setState(() {
+          _isInitialized = true; //MARK: test목적으로 true설정
+        });
       }
     } catch (e) {
       print("카메라 초기화 중 예외 발생 (시뮬레이터 예상): $e");
-      _moveToPreviewScreen();
+      //MARK: test
+      // _moveToPreviewScreen(testVideoPath);
+
+      print("카메라 초기화 예외 발생 -> UI 점검을 위해 화면 유지: $e");
+      setState(() {
+        _isInitialized = true;
+      });
     }
   }
 
-  //MARK: moveToPreview
-  void _moveToPreviewScreen() {
+  //MARK: MoveToPreview
+  void _moveToPreviewScreen(String recordedPath) {
     if (!mounted) return;
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) =>
-            const VideoPreviewScreen(videoPath: 'assets/video/test_video.mp4'),
+        builder: (context) => VideoPreviewScreen(videoPath: recordedPath),
       ),
     );
   }
 
-  //MARK: Camera dispose
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-
-  //MARK: Shutter Button
-  Widget _buildCaptureButton() {
+  //MARK: Button UI
+  Widget _buildRecordButton() {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Padding(
-        padding: const EdgeInsets.only(bottom: 30),
+        padding: const EdgeInsets.only(bottom: 50),
         child: GestureDetector(
-          onTap: _takePicture,
-          child: Container(
-            height: 70,
-            width: 70,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: Colors.white, width: 4),
-            ),
-            child: const Center(
-              child: Icon(Icons.camera_alt, color: Colors.white, size: 30),
-            ),
+          onTap: _recordVideo,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 4),
+                ),
+              ),
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                height: _isRecording ? 30 : 60,
+                width: _isRecording ? 30 : 60,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(_isRecording ? 5 : 30),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Future<void> _takePicture() async {
-    if (_controller == null || !_controller!.value.isInitialized) return;
-
-    try {
-      final image = await _controller!.takePicture();
-      print("사진 저장 경로: ${image.path}");
-
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('사진이 촬영되었습니다!')));
-    } catch (e) {
-      print("촬영 에러: $e");
-      WarningSnackBar.showWarning(context, "촬영에 실패했습니다.");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('${widget.groupName} 촬영'),
-        backgroundColor: Colors.black,
-      ),
       body: _isInitialized
           ? Stack(
               children: [
-                Center(child: CameraPreview(_controller!)),
-                _buildCaptureButton(),
+                Positioned.fill(
+                  child:
+                      (_controller != null && _controller!.value.isInitialized)
+                      ? ClipRect(
+                          child: OverflowBox(
+                            alignment: Alignment.center,
+                            child: FittedBox(
+                              fit: BoxFit.cover,
+                              child: SizedBox(
+                                width: _controller!.value.previewSize!.height,
+                                height: _controller!.value.previewSize!.width,
+                                child: CameraPreview(_controller!),
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(
+                          color: const Color.fromARGB(255, 208, 146, 146),
+                          child: const Center(
+                            child: Text(
+                              "시뮬레이터: 카메라 없음",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                ),
+                _buildRecordButton(),
               ],
             )
           : const Center(child: CircularProgressIndicator(color: Colors.white)),
